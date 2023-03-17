cf5cceadc98d94d1d00e83e327648867d9d1edb9
SRISANTHOSH SEKAR <83419129+SRISANTHOSH-SEKAR@users.noreply.github.com>
2023-03-17 23:36:53 +0530

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -13,7 +13,7 @@ import services_logo2 from './assets/images/Services/logo2.png';
 import services_logo3 from './assets/images/Services/logo3.png';
 
 import project1 from './assets/images/Projects/img1.png';
-import project2 from './assets/images/projects/img2.png';
+import project2 from './assets/images/Projects/img2.png';
 import project3 from './assets/images/Projects/img3.png';
 import person_project from './assets/images/Projects/person.png';
 
